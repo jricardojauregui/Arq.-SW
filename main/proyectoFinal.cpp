0e0e775866256cc6f7d6620ac9a6c6513fdaae8b
José Ricardo Jáuregui Guevara <jose.jauregui@udem.edu>
2025-04-22 12:29:04 -0600

    Creación de main

---
+++ main/proyectoFinal.cpp
@@ -0,0 +1,23 @@
+/*
+Programa                  proyectoFinal
+Nom. materia              Arquitectura de Software
+Nom. prof.                Dr. Felipe de Jesús Rdz.
+Fecha creac.              22/04/25
+Fecha ult. mod.           99/99/99
+
+Bitacora. de mant.
+Nom: Santiago López       Fech. Mod.: 99/99/99       Desc: descripcionMod 
+Nom: Ricardo Jáuregui     Fech. Mod.: 99/99/99       Desc: descripcionMod 
+Nom: Paola Loredo         Fech. Mod.: 99/99/99       Desc: descripcionMod 
+Nom: Fabían Orta          Fech. Mod.: 99/99/99       Desc: descripcionMod 
+       
+Descripción del funcionamiento del programa:
+descripcionCorta1
+*/
+
+#include "iostream"
+
+int main(){
+
+    return 0;
+}
