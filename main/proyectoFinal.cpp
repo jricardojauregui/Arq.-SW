afde6cdd67692694ac7c3552ce8e81fa7b3a8271
José Ricardo Jáuregui Guevara <jose.jauregui@udem.edu>
2025-04-28 15:38:12 -0600

    Avances

--- main/proyectoFinal.cpp
+++ main/proyectoFinal.cpp
@@ -57,17 +57,15 @@ static ifstream finMov;
 static ofstream foutNewPers;
 static ofstream foutReporte;
 
-// Control EOF
-typedef bool Flag;
-static Flag eofPers = false;
-static Flag eofMov  = false;
+static bool eofPers = false;
+static bool eofMov  = false;
 
-// Registros en memoria
 static Personal per;
 static Movimiento mov;
 
-typedef istringstream ISS;
-string leerCampo(ISS &ss) {
+static bool existe = false;
+
+string leerCampo(istringstream &ss) {
     string f;
     getline(ss, f, ',');
     return f;
@@ -76,32 +74,30 @@ string leerCampo(ISS &ss) {
 void abreArchivos() {
     finPers.open("datosExistentesPER.txt");
     finMov.open("datosExistentesMOV.txt");
-    foutNewPers.open("nuevo_personal.txt");
+    foutNewPers.open("nuevoPersonal.txt");
     foutReporte.open("reporte.txt");
-    
-    if (!finPers || !finMov) 
-    {
+    if (!finPers || !finMov) {
         cerr << "Error al abrir archivos de entrada" << endl;
         exit(EXIT_FAILURE);
     }
 }
 
-/*
-Objetivo leeMovimiento(): Leer los tipos de movimiento que se pueden realizar dado un archivo
-*/
-
-void leeMovimiento(){
+void leeMovimiento() {
     string linea;
-    if (getline(finMov, linea)) 
-    {
-        ISS ss(linea);
+    if (getline(finMov, linea)) {
+        istringstream ss(linea);
         mov.CVE = leerCampo(ss)[0];
         mov.TRAB = leerCampo(ss);
         mov.GPO = leerCampo(ss);
         mov.EMP = leerCampo(ss);
         mov.PTA = leerCampo(ss);
         mov.DEPTO = leerCampo(ss);
-        mov.CLAVE = leerCampo(ss)[0];
+        string tmp = leerCampo(ss);
+        if (tmp.empty()) {
+            mov.CLAVE = 'O';
+        } else {
+            mov.CLAVE = tmp[0];
+        }
         mov.NOMB = leerCampo(ss);
         mov.SAL = stod(leerCampo(ss));
         mov.F_ING = stoi(leerCampo(ss));
@@ -111,17 +107,21 @@ void leeMovimiento(){
     }
 }
 
-void leePersonal(){
+void leePersonal() {
     string linea;
-    if (getline(finPers, linea)) 
-    {
-        ISS ss(linea);
+    if (getline(finPers, linea)) {
+        istringstream ss(linea);
         per.TRAB = leerCampo(ss);
         per.GPO = leerCampo(ss);
         per.EMP = leerCampo(ss);
         per.PTA = leerCampo(ss);
         per.DEPTO = leerCampo(ss);
-        per.CLAVE = leerCampo(ss)[0];
+        string tmpClave = leerCampo(ss);
+        if (tmpClave.empty()) {
+            per.CLAVE = 'O';
+        } else {
+            per.CLAVE = tmpClave[0];
+        }
         per.NOMB = leerCampo(ss);
         per.SAL = stod(leerCampo(ss));
         per.F_ING = stoi(leerCampo(ss));
@@ -131,174 +131,153 @@ void leePersonal(){
     }
 }
 
-void alta() {
-    Personal np;
-    np.TRAB = mov.TRAB;
-
-    if (!mov.GPO.empty()) 
-    {
-        np.GPO = mov.GPO;
-    } else {
-        np.GPO = "G000";
-    }
-
-    if (!mov.EMP.empty()) 
-    {
-        np.EMP = mov.EMP;
-    } else {
-        np.EMP = "E000";
-    }
-
-    if (!mov.PTA.empty()) 
-    {
-        np.PTA = mov.PTA;
-    } else {
-        np.PTA = "P000";
-    }
-
-    if (!mov.DEPTO.empty()) 
-    {
-        np.DEPTO = mov.DEPTO;
-    } else {
-        np.DEPTO = "D000";
-    }
-
-    if (mov.CLAVE != 'O') 
-    {
-        np.CLAVE = mov.CLAVE;
-    } else {
-        np.CLAVE = 'O';
-    }
-
-    if (!mov.NOMB.empty()) {
-        np.NOMB = mov.NOMB;
-    } else {
-        np.NOMB = "N000";
-    }
-
-    if (mov.SAL != 0.0) 
-    {
-        np.SAL = mov.SAL;
-    } else {
-        np.SAL = 1000.0;
-    }
-
-    if (mov.F_ING != 0) 
-    {
-        np.F_ING = mov.F_ING;
-    } else {
-        time_t ahora = time(0);
-        tm* ltm = localtime(&ahora);
-
-        np.F_ING = (1900 + ltm->tm_year) * 10000 + (1 + ltm->tm_mon) * 100 + ltm->tm_mday;
-    }
-
-    foutNewPers << np.TRAB << ',' << np.GPO << ',' << np.EMP << ',' << np.PTA << ',' << np.DEPTO << ',' << np.CLAVE << ',' << np.NOMB << ',' << fixed << setprecision(2) << np.SAL << ',' << np.F_ING << '\n';
-    foutReporte << np.TRAB << " ALTA EXITOSA\n";
-}
-
-
-void cambio(){
-    if (!mov.GPO.empty()) 
-    {
-        per.GPO = mov.GPO;
-    }
-    if (!mov.EMP.empty()) 
-    {
-        per.EMP = mov.EMP;
-    }
-    if (!mov.PTA.empty()) 
-    {
-        per.PTA = mov.PTA;
-    }
-    if (!mov.DEPTO.empty()) 
-    {
-        per.DEPTO = mov.DEPTO;
-    }
-    if (mov.CLAVE != 'O') 
-    { 
-        per.CLAVE = mov.CLAVE;
-    }
-    if (!mov.NOMB.empty()) 
-    {
-        per.NOMB = mov.NOMB;
-    }
-    if (mov.SAL != 0.0) 
-    {
-        per.SAL = mov.SAL;
-    }
-    if (mov.F_ING != 0) 
-    {
-        per.F_ING = mov.F_ING;
-    }
-
-    foutNewPers << per.TRAB << ',' << per.GPO << ',' << per.EMP << ',' << per.PTA << ',' << per.DEPTO << ',' << per.CLAVE << ',' << per.NOMB << ',' << fixed << setprecision(2) << per.SAL << ',' << per.F_ING << '\n';
-    foutReporte << per.TRAB << " CAMBIO EXITOSO\n";
-       
-}
-
-void baja(){
-       foutReporte << mov.TRAB << " BAJA EXITOSA\n";
-}
-
 void copia() {
     foutNewPers << per.TRAB << ',' << per.GPO << ',' << per.EMP << ',' << per.PTA << ',' << per.DEPTO << ',' << per.CLAVE << ',' << per.NOMB << ',' << fixed << setprecision(2) << per.SAL << ',' << per.F_ING << '\n';
 }
 
-
-void movimientoPersonal(){
-    switch(mov.CVE) 
-    {
-    case 'A': 
-        alta();
-        break;
-    case 'B': 
-        baja(); 
-        break;
-    case 'C': 
-        cambio(); 
-        break;
-    default: 
-        foutReporte << "MOVIMIENTO INVALIDO: " << mov.TRAB << '\n';
-        break;
+void alta() {
+    if (existe) {
+        foutReporte << mov.TRAB << " ALTA NO EXITOSA" << '\n';
+        copia();
+    } else {
+        Personal np;
+        np.TRAB = mov.TRAB;
+        if (mov.GPO.empty()) {
+            np.GPO = "G000";
+        } else {
+            np.GPO = mov.GPO;
+        }
+        if (mov.EMP.empty()) {
+            np.EMP = "E000";
+        } else {
+            np.EMP = mov.EMP;
+        }
+        if (mov.PTA.empty()) {
+            np.PTA = "P000";
+        } else {
+            np.PTA = mov.PTA;
+        }
+        if (mov.DEPTO.empty()) {
+            np.DEPTO = "D000";
+        } else {
+            np.DEPTO = mov.DEPTO;
+        }
+        if (mov.CLAVE != 'O') {
+            np.CLAVE = mov.CLAVE;
+        } else {
+            np.CLAVE = 'O';
+        }
+        if (mov.NOMB.empty()) {
+            np.NOMB = "N000";
+        } else {
+            np.NOMB = mov.NOMB;
+        }
+        if (mov.SAL != 0.0) {
+            np.SAL = mov.SAL;
+        } else {
+            np.SAL = 1000.0;
+        }
+        if (mov.F_ING != 0) {
+            np.F_ING = mov.F_ING;
+        } else {
+            time_t t = time(nullptr);
+            tm* lt = localtime(&t);
+            np.F_ING = (1900 + lt->tm_year) * 10000 + (1 + lt->tm_mon) * 100 + lt->tm_mday;
+        }
+        foutNewPers << np.TRAB << ',' << np.GPO << ',' << np.EMP << ',' << np.PTA << ',' << np.DEPTO << ',' << np.CLAVE << ',' << np.NOMB << ',' << fixed << setprecision(2) << np.SAL << ',' << np.F_ING << '\n';
+        foutReporte << mov.TRAB << " ALTA EXITOSA" << '\n';
     }
 }
 
-void termina(){
+void baja() {
+    if (existe) {
+        foutReporte << mov.TRAB << " BAJA EXITOSA" << '\n';
+    } else {
+        foutReporte << mov.TRAB << " BAJA NO EXITOSA" << '\n';
+    }
+}
+
+void cambio() {
+    if (existe) {
+        if (!mov.GPO.empty()) {
+            per.GPO = mov.GPO;
+        }
+        if (!mov.EMP.empty()) {
+            per.EMP = mov.EMP;
+        }
+        if (!mov.PTA.empty()) {
+            per.PTA = mov.PTA;
+        }
+        if (!mov.DEPTO.empty()) {
+            per.DEPTO = mov.DEPTO;
+        }
+        if (mov.CLAVE != 'O') {
+            per.CLAVE = mov.CLAVE;
+        }
+        if (!mov.NOMB.empty()) {
+            per.NOMB = mov.NOMB;
+        }
+        if (mov.SAL != 0.0) {
+            per.SAL = mov.SAL;
+        }
+        if (mov.F_ING != 0) {
+            per.F_ING = mov.F_ING;
+        }
+        foutNewPers << per.TRAB << ',' << per.GPO << ',' << per.EMP << ',' << per.PTA << ',' << per.DEPTO << ',' << per.CLAVE << ',' << per.NOMB << ',' << fixed << setprecision(2) << per.SAL << ',' << per.F_ING << '\n';
+        foutReporte << mov.TRAB << " CAMBIO EXITOSO" << '\n';
+    } else {
+        foutReporte << mov.TRAB << " CAMBIO NO EXITOSO" << '\n';
+    }
+}
+
+void movimientoPersonal() {
+    switch (mov.CVE) {
+        case 'A': 
+            alta();   
+            break;
+        case 'B': 
+            baja();   
+            break;
+        case 'C': 
+            cambio(); 
+            break;
+        default:
+            foutReporte << mov.TRAB << " MOVIMIENTO INVALIDO" << '\n';
+    }
+}
+
+void termina() {
     finPers.close();
     finMov.close();
     foutNewPers.close();
     foutReporte.close();
 }
 
-int main(){
+int main() {
     abreArchivos();
     leeMovimiento();
     leePersonal();
 
     while (!eofMov || !eofPers) {
-        if (!eofMov && !eofPers) 
-        {
-            if (mov.TRAB == per.TRAB) 
-            {
+        if (!eofMov && !eofPers) {
+            if (mov.TRAB == per.TRAB) {
+                existe = true;
                 movimientoPersonal();
                 leeMovimiento();
                 leePersonal();
-            } 
-            else if (mov.TRAB < per.TRAB) {
-                alta();
+            } else if (mov.TRAB < per.TRAB) {
+                existe = false;
+                movimientoPersonal();
                 leeMovimiento();
-            } 
-            else {
+            } else {
                 copia();
                 leePersonal();
             }
-        } 
-        else if (!eofMov) {
-            alta();
+        } else if (!eofMov) {
+            existe = false;
+            movimientoPersonal();
             leeMovimiento();
-        } 
-        else {
+        } else {
             copia();
             leePersonal();
         }
