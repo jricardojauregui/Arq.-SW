56f344926ef56c9bb72f3a0e55a795a5f7f7f0c9
José Ricardo Jáuregui Guevara <jose.jauregui@udem.edu>
2025-04-27 20:39:34 -0600

    Alta

--- main/proyectoFinal.cpp
+++ main/proyectoFinal.cpp
@@ -17,7 +17,9 @@ descripcionCorta1
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <iomanip>
+#include <ctime>
 
 using namespace std;
 
@@ -38,6 +40,43 @@ main()                  Implementar funciones existentes
 /*
 Objetivo leeMovimiento(): Leer los tipos de movimiento que se pueden realizar dado un archivo
 */
+
+struct Personal {
+    string TRAB, GPO, EMP, PTA, DEPTO, NOMB;
+    char CLAVE;
+    double SAL;
+    int F_ING;
+};
+
+struct Movimiento {
+    char CVE;
+    string TRAB, GPO, EMP, PTA, DEPTO, NOMB;
+    char CLAVE;
+    double SAL;
+    int F_ING;
+};
+
+static ifstream finPers;
+static ifstream finMov;
+static ofstream foutNewPers;
+static ofstream foutReporte;
+
+// Control EOF
+typedef bool Flag;
+static Flag eofPers = false;
+static Flag eofMov  = false;
+
+// Registros en memoria
+static Personal persRec;
+static Movimiento movRec;
+
+typedef istringstream ISS;
+string leerCampo(ISS &ss) {
+    string f;
+    getline(ss, f, ',');
+    return f;
+}
+
 void leeMovimiento(){
 
 }
@@ -46,9 +85,65 @@ void leePersonal(){
 
 }
 
-void alta(){
-    
+void alta() {
+    Personal np;
+    np.TRAB = movRec.TRAB;
+
+    if (!movRec.GPO.empty()) {
+        np.GPO = movRec.GPO;
+    } else {
+        np.GPO = "G000";
+    }
+
+    if (!movRec.EMP.empty()) {
+        np.EMP = movRec.EMP;
+    } else {
+        np.EMP = "E000";
+    }
+
+    if (!movRec.PTA.empty()) {
+        np.PTA = movRec.PTA;
+    } else {
+        np.PTA = "P000";
+    }
+
+    if (!movRec.DEPTO.empty()) {
+        np.DEPTO = movRec.DEPTO;
+    } else {
+        np.DEPTO = "D000";
+    }
+
+    if (movRec.CLAVE != 'O') {
+        np.CLAVE = movRec.CLAVE;
+    } else {
+        np.CLAVE = 'O';
+    }
+
+    if (!movRec.NOMB.empty()) {
+        np.NOMB = movRec.NOMB;
+    } else {
+        np.NOMB = "N000";
+    }
+
+    if (movRec.SAL != 0.0) {
+        np.SAL = movRec.SAL;
+    } else {
+        np.SAL = 1000.0;
+    }
+
+    if (movRec.F_ING != 0) {
+        np.F_ING = movRec.F_ING;
+    } else {
+        time_t now = time(0);
+        tm* ltm = localtime(&now);
+
+        np.F_ING = (1900 + ltm->tm_year) * 10000 + (1 + ltm->tm_mon) * 100 + ltm->tm_mday;
+    }
+
+    foutNewPers << np.TRAB << ',' << np.GPO << ',' << np.EMP << ',' << np.PTA << ',' << np.DEPTO << ',' << np.CLAVE << ',' << np.NOMB << ',' << fixed << setprecision(2) << np.SAL << ',' << np.F_ING << '\n';
+    foutReporte << np.TRAB << " ALTA EXITOSA\n";
 }
+
 
 void cambio(){}
 
